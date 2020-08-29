9bb415c680877376b2f49886abbc56b541c438d9
TImA97 <tim.angelike@gmx.de>
2020-08-29 08:48:46 +0200

    Add comments and improve code

--- R/redundancy.R
+++ R/redundancy.R
@@ -5,20 +5,23 @@
 #' @return redundancy index of \code{x}
 redundancy_index <- function(x, possible_responses) {
   if (possible_responses < 2) {
-    stop('sequence must include at least 2 responses')
+    stop('sequence must include at least 2 distinct responses')
   }
 
   x <- to_numeric(x)
-  unique_responses <- length(unique(x))
-  freq_table <- table(x)
-  frequencies <- as.vector(freq_table)
+  distinct_responses <- length(unique(x))
+  frequencies <- as.vector(table(x))
 
-  if (possible_responses < unique_responses) {
+  # check whether there are more distinct options in the vector than declared
+  # if so, return error message.
+  # if there are more possible  than distinct options in the vector,
+  # add the omitted options to the observed frequencies with value '0'
+  if (possible_responses < distinct_responses) {
     stop('vector contains more unique responses than declared in function call')
-  } else if (possible_responses > unique_responses) {
-    options_to_be_added <- possible_responses - unique_responses
+  } else if (possible_responses > distinct_responses) {
+    options_to_be_added <- possible_responses - distinct_responses
     for (i in 1:options_to_be_added) {
-      frequencies[unique_responses + i] <- 0
+      frequencies[distinct_responses + i] <- 0
     }
   }
 
