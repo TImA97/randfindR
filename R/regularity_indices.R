2884c27392761f4870653ddae5ea32282ae955fc
TImA97 <tim.angelike@gmx.de>
2020-09-16 16:19:51 +0200

    Add new file regularity_indices including implementation of runs_index

---
+++ R/regularity_indices.R
@@ -0,0 +1,28 @@
+#' Compute runs (see Ginsburg & Karpiuk, 1994)
+#' @description Compute the variance of successive ascending or descending runs
+#' @param x vector of random numbers
+#' @return runs of \code{x}
+runs_index <- function(x, asc = TRUE) {
+  x <- to_numeric(x)
+
+  runs <- vector(length = 1)
+
+  # iterate over vector and count runs
+  current_length <- 1
+  runs_counter <- 1
+  for (i in 2:length(x)) {
+    if (x[i] > x[i - 1]) {
+      current_length <- current_length + 1
+    } else {
+      runs[runs_counter] <- current_length
+      current_length <- 1
+      runs_counter <- runs_counter + 1
+    }
+  }
+
+  # store length of last run
+  runs[runs_counter] <- current_length
+  current_length <- 0
+
+  return(runs)
+}
