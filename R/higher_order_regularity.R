9df9c879b28bdc40f52a633cfd37a5a8bdd30229
TImA97 <tim.angelike@gmx.de>
2020-09-17 16:57:45 +0200

    Add file higher_order_regularity

---
+++ R/higher_order_regularity.R
@@ -0,0 +1,5 @@
+#' Compute regularity index by Skliar, Monge & Oviedo (2009)
+#' @param x vector of random numbers
+#' @param options number of available options in sequence
+#' @return regularity index of \code{x}
+
