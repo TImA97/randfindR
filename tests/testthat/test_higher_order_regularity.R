d4eeba56676c28e1ae288cff2485d46bbb44c58c
TImA97 <tim.angelike@gmx.de>
2020-09-19 08:56:39 +0200

    Add two more tests for reg_index

--- tests/testthat/test_higher_order_regularity.R
+++ tests/testthat/test_higher_order_regularity.R
@@ -3,4 +3,6 @@ context("higher order regularity")
 
 test_that("regularity index retuns correct result", {
   expect_equal(reg_index(rep(1, times = 20), 2), 1)
+  expect_equal(round(reg_index(rep(c(0, 1), each = 10), 2), 4), 1 - 0.4226)
+  expect_equal(round(reg_index(rep(c(0, 1), times = 10), 2), 4), 1 - 0.4226)
 })
