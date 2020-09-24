4eb5a433ee85b3ac3883d244930a9ec2a3e3e110
TImA97 <tim.angelike@gmx.de>
2020-09-24 13:21:58 +0200

    Add check for vector length to runs_index and slightly modify existing code

--- R/regularity_indices.R
+++ R/regularity_indices.R
@@ -4,11 +4,12 @@
 #' @return runs of \code{x}
 runs_index <- function(x, asc = TRUE) {
   x <- to_numeric(x)
+  is_vector_long_enough(x)
 
-  runs <- vector(length = 1)
+  runs <- numeric(length = 1)
 
   # iterate over vector and count runs
-  # make case distinction whether asc is 'TRUE' or 'FALSE'
+  # make case distinction whether 'asc' is 'TRUE' or 'FALSE'
   current_length <- 1
   runs_counter <- 1
   for (i in 2:length(x)) {
@@ -25,7 +26,6 @@ runs_index <- function(x, asc = TRUE) {
 
   # store length of last run
   runs[runs_counter] <- current_length
-  current_length <- 0
 
   # compute population variance of runs
   variance <- var(runs) * (length(runs) - 1) / length(runs)
