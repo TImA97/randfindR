4e194b6136db87fbdc8313df490730b8b4a9d5fd
TImA97 <tim.angelike@gmx.de>
2020-09-24 12:26:38 +0200

    Add check for vector length (should be at least 2)

--- R/check_service.R
+++ R/check_service.R
@@ -2,8 +2,16 @@
 
 # aggregate function that conducts basic checks for functions
 base_checks <- function(x, options, min_options) {
+  vector_length_at_least_two(x);
   sufficient_options_provided(options, min_options)
   is_number_of_distinct_options_too_high(x, options)
+}
+
+# check whether vector length is at least 2
+vector_length_at_least_two <- function(x) {
+  if (length(x) < 2) {
+    stop("sequence length should be at least 2")
+  }
 }
 
 # check whether the provided number of options exceeds the minimum number of
