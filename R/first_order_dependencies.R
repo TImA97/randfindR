a023bbf3e7b29a1d08e5cfef297aab712592bfb0
TImA97 <tim.angelike@gmx.de>
2020-10-04 09:31:04 +0200

    Move rng_index to separate file

--- R/first_order_dependencies.R
+++ R/first_order_dependencies.R
@@ -27,42 +27,6 @@ digram_rep <- function(x, options) {
   return(sum)
 }
 
-#' Compute RNG index by Evans (1978)
-#'
-#' @param x vector of random numbers
-#' @param options number of available options in sequence
-#' @return RNG index of \code{x}
-#'
-#' @export
-rng_index <- function(x, options) {
-
-
-  x <- to_numeric(x)
-  min_options <- 2
-  base_checks(x, options, min_options)
-  matr <- convert_to_matrix(x, options)
-
-  dividend  <- 0
-  for (i in 1:options) {
-    for (j in 1:options) {
-      if (matr[i,j] > 1) {
-        dividend  <- dividend  + log10(matr[i,j]) *  matr[i,j]
-      }
-    }
-  }
-
-  divisor <- 0
-  row_marginals <- rowSums(matr)
-  for (i in 1:options) {
-    row_marginal <- row_marginals[i]
-    if (row_marginal > 1) {
-      divisor <- divisor + log10(row_marginal) * row_marginal;
-    }
-  }
-
-  result <- dividend / divisor
-  return(result)
-}
 
 #' Compute repetitions as measure of randomness (see Ginsburg & Karpiuk, 1994)
 #' @description Compute frequency a value is repeated in the next round
