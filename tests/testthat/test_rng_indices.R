f5446863a099f860e70c1beac6561eddc856c675
TImA97 <tim.angelike@gmx.de>
2020-10-15 10:26:24 +0200

    Add (skipped) tests

--- tests/testthat/test_rng_indices.R
+++ tests/testthat/test_rng_indices.R
@@ -5,3 +5,10 @@ test_that("rng_index returns correct result", {
   expect_equal(round(rng_index(evans1978$sequence_two, 10), 3), 0.427)
   expect_equal(round(rng_index(ginsburg1994, 10), 2), 0.34)
 })
+
+test_that("rng2_index returns correct result", {
+  skip('skip until differences are resolved. Test data might be flawed.')
+  expect_equal(round(rng2_index(evans1978$sequence_one, 10), 4), 0.2551)
+  expect_equal(round(rng2_index(evans1978$sequence_two, 10), 4), 0.2652)
+  expect_equal(round(rng2_index(ginsburg1994, 10), 4), 0.2939)
+})
