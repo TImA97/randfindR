70c1562fb0e87daab6353e8870af3c64b5a876be
TImA97 <tim.angelike@gmx.de>
2020-10-15 09:07:51 +0200

    Move code for testing rng_index to the correct file

--- tests/testthat/test_first_order_dependencies.R
+++ tests/testthat/test_first_order_dependencies.R
@@ -1,12 +1,5 @@
 context("first order indizes of randomness")
 
-test_that("rng_index returns correct result", {
-  expect_equal(round(rng_index(evans1978$sequence_one, 10), 3), 0.216)
-  expect_equal(round(rng_index(evans1978$sequence_two, 10), 3), 0.427)
-  expect_equal(round(rng_index(ginsburg1994, 10), 2), 0.34)
-
-})
-
 test_that("digram_rep returns correct result", {
   expect_equal(digram_rep(ginsburg1994, 10), 47)
 })
--- tests/testthat/test_rng_indices.R
+++ tests/testthat/test_rng_indices.R
@@ -1,2 +1,7 @@
 context("RNG indices of randomness")
 
+test_that("rng_index returns correct result", {
+  expect_equal(round(rng_index(evans1978$sequence_one, 10), 3), 0.216)
+  expect_equal(round(rng_index(evans1978$sequence_two, 10), 3), 0.427)
+  expect_equal(round(rng_index(ginsburg1994, 10), 2), 0.34)
+})
