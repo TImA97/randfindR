93dffd64d471531577291b42c79613901d4d82b8
TImA97 <tim.angelike@gmx.de>
2020-10-19 11:02:27 +0200

    Add first version of documentation for phi_index

---
+++ R/phi_index.R
@@ -0,0 +1,23 @@
+#' Phi Index
+#'
+#' @param x vector of random numbers
+#' @param options number of available options in sequence
+#' @param order order of analysis
+#' @return Phi index of \code{x}
+#'
+#' @details
+#' to be filled
+#'
+#' @export
+#'
+#' @references
+#' Ginsburg N, Karpiuk P. Random Generation: Analysis of the Responses.
+#' Perceptual and Motor Skills. 1994;79(3):1059-1067.
+#' \url{doi:10.2466/pms.1994.79.3.1059}
+#'
+#' Wagenaar, W. A. 1970. “Subjective Randomness and the Capacity to
+#' Generate Information.” Acta Psychologica 33:233–42.
+#' doi: 10.1016/0001-6918(70)90135-6.
+phi_index <- function(x, options, order = 6) {
+
+}
