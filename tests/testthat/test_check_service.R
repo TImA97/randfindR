35d20e1178319b63ba20d9558827be565e25dbb8
TImA97 <tim.angelike@gmx.de>
2020-10-29 17:36:39 +0100

    Restructure tests for check_service

--- tests/testthat/test_check_service.R
+++ tests/testthat/test_check_service.R
@@ -1,17 +1,15 @@
 context("tests by check_service")
 
-test_that("is_vector_long_enough returns correct error", {
+test_that("checks return errors", {
   expect_error(is_vector_long_enough(1))
   expect_error(is_vector_long_enough(1:10, min_length = 11))
-  expect_error(is_vector_long_enough(1:10, min_length = 10), NA)
-})
-
-test_that("sufficient_options_provided returns error", {
   expect_error(sufficient_options_provided(options = 3, min_options = 4))
-  expect_error(sufficient_options_provided(options = 3, min_options = 3), NA)
+  expect_error(is_number_of_distinct_options_too_high(1:4, options = 3))
 })
 
-test_that("is_number_of_distinct_options_too_high returns error", {
-  expect_error(is_number_of_distinct_options_too_high(1:4, options = 3))
+test_that("checks return no error", {
+  expect_error(sufficient_options_provided(options = 3, min_options = 3), NA)
+  expect_error(is_vector_long_enough(1:10, min_length = 10), NA)
   expect_error(is_number_of_distinct_options_too_high(1:4, options = 4), NA)
 })
+
