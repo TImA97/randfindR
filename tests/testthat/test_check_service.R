1e17d272888caf9b31f106aef633bea35c522c5e
TImA97 <tim.angelike@gmx.de>
2020-10-29 17:33:20 +0100

    Add tests for check_service

--- tests/testthat/test_check_service.R
+++ tests/testthat/test_check_service.R
@@ -1,1 +1,17 @@
 context("tests by check_service")
+
+test_that("is_vector_long_enough returns correct error", {
+  expect_error(is_vector_long_enough(1))
+  expect_error(is_vector_long_enough(1:10, min_length = 11))
+  expect_error(is_vector_long_enough(1:10, min_length = 10), NA)
+})
+
+test_that("sufficient_options_provided returns error", {
+  expect_error(sufficient_options_provided(options = 3, min_options = 4))
+  expect_error(sufficient_options_provided(options = 3, min_options = 3), NA)
+})
+
+test_that("is_number_of_distinct_options_too_high returns error", {
+  expect_error(is_number_of_distinct_options_too_high(1:4, options = 3))
+  expect_error(is_number_of_distinct_options_too_high(1:4, options = 4), NA)
+})
