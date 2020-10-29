1e3914de0615513b3449094e261aa1d6b1968c28
TImA97 <tim.angelike@gmx.de>
2020-10-29 17:20:55 +0100

    Update documentation of check_service

--- R/check_service.R
+++ R/check_service.R
@@ -1,14 +1,24 @@
-# this class contains checks that are conducted before the main code is executed
 
-# aggregate function that conducts basic checks for functions
-# set default of min_length to 2
+
+#' Aggregate function that bundles main checks together
+#' @param x vector of random numbers
+#' @param options number of provided options
+#' @param min_options number of minimum options for function to work
+#' @param min_length minimum vector length for function to work
+#'
+#' @noRd
 base_checks <- function(x, options, min_options, min_length = 2) {
   is_vector_long_enough(x, min_length);
   sufficient_options_provided(options, min_options)
   is_number_of_distinct_options_too_high(x, options)
 }
 
-# check whether vector length is at least 2
+#' Check whether provided vector has a length of at least 2
+#'
+#' @param x vector of random numbers
+#' @param min_length minimum vector length for function to work
+#'
+#' @noRd
 is_vector_long_enough <- function(x, min_length = 2) {
   if (length(x) < min_length) {
     error_message <- paste("the sequence should contain at least",
@@ -18,8 +28,13 @@ is_vector_long_enough <- function(x, min_length = 2) {
   }
 }
 
-# check whether the provided number of options exceeds the minimum number of
-# options for the function to work
+#' Check whether the provided number of options is high enough for the function
+#' to work
+#'
+#' @param options number of provided options
+#' @param min_options number of minimum options for function to work
+#'
+#' @noRd
 sufficient_options_provided <- function(options, min_options) {
 
   if (options < min_options) {
@@ -29,8 +44,13 @@ sufficient_options_provided <- function(options, min_options) {
   }
 }
 
-# check whether vector contains more unique options than provided in the
-# 'options' argument
+#' Check whether there are more unique options in the provided vector than
+#' specified in options
+#'
+#' @param x vector of random numbers
+#' @param options number of provided options
+#'
+#' @noRd
 is_number_of_distinct_options_too_high <- function(x, options) {
   distinct_options <- get_number_unique_responses(x)
 
