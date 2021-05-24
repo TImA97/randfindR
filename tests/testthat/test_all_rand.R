680726fa2cd7f52e2590af9cc8998f1e3946cfc0
TImA97 <tim.angelike@gmx.de>
2021-05-24 15:44:06 +0200

    Add third test for all_rand

--- tests/testthat/test_all_rand.R
+++ tests/testthat/test_all_rand.R
@@ -3,4 +3,11 @@ context("Test aggregate function all_rand")
 
 test_that("returned vector has correct length", {
   expect_equal(length(all_rand(ginsburg1994, 10)), 15)
+  expect_equal(length(all_rand(ginsburg1994, 10, indices = "reg_index")), 1)
+  expect_equal(length(all_rand(
+    ginsburg1994,
+    options = 10,
+    indices = c("reg_index", "rng_index", "runs_index")
+  )), 3)
+
 })
